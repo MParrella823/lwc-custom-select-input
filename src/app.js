c0437988e19f5ccebf661a0d04911693b82ffb05
Matt Parrella <21349048+undefined@users.noreply.github.com>
2022-03-08 19:44:38 +0000

    fully working this time :)

--- src/app.js
+++ src/app.js
@@ -154,17 +154,22 @@ export default class App extends LightningElement {
     let tailClone = [];
     dataClone = JSON.parse(JSON.stringify(this.apgTails));
     let picklistClone = [];
+    let selectedListTag;
     dataClone.forEach(item => {
       if (item.quoteLineId === event.target.dataset.id){
+        selectedListTag = item.listTag;
         item.isRegistered = false;
         picklistClone = JSON.parse(JSON.stringify(this.picklistMaps.get(item.listTag)));
         tailClone = this.picklistMaps.get(item.listTag);
+        console.log('existing tailClone: ', tailClone);
         tailClone.push(item.selectedTail);
+        console.log('updated tailClone: ', tailClone);
         item.selectedTail = {label: '', value: null};
+        this.picklistMaps.set(item.listTag, tailClone);
       }
       
-    this.picklistMaps.set(item.listTag, tailClone);
-    item.sysTailWrappers = this.picklistMaps.get(item.listTag);
+      console.log('final list tag: ', this.picklistMaps.get(item.listTag));
+      item.sysTailWrappers = JSON.parse(JSON.stringify(this.picklistMaps.get(item.listTag)));
 
     });
     this.apgTails = dataClone;
